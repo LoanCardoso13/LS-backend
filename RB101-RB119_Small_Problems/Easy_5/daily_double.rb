e84f6e3d256d094187c89e72c8bb796a685955d9
Loan Cardoso <jow13_122@yahoo.com>
2023-11-14 13:13:33 -0500

    Easy_5 problems set up

---
+++ RB101-RB119_Small_Problems/Easy_5/daily_double.rb
@@ -0,0 +1,21 @@
+=begin
+
+	Write a method that takes a string argument and returns a new string that contains the value of the original string with all consecutive duplicate characters collapsed into a single character. You may not use String#squeeze or String#squeeze!.
+
+PEDAC
+
+Problem: 
+
+Examples and test cases:
+
+	crunch('ddaaiillyy ddoouubbllee') == 'daily double'
+	crunch('4444abcabccba') == '4abcabcba'
+	crunch('ggggggggggggggg') == 'g'
+	crunch('a') == 'a'
+	crunch('') == ''
+
+Data structure:
+
+Algorithm:
+
+=end
