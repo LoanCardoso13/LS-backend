1dd090568f5eec78c5910d44074f7ac01fed93da
Loan Cardoso <jow13_122@yahoo.com>
2023-11-16 13:18:19 -0500

    Easy_6 problems set up

---
+++ RB101-RB119_Small_Problems/Easy_6/combining_arrays.rb
@@ -0,0 +1,31 @@
+=begin 
+
+	Write a method that takes two Arrays as arguments, and returns an Array that contains all of the values from the argument Arrays. There should be no duplication of values in the returned Array, even if there are duplicates in the original Arrays.
+
+PEDAC
+
+Problem:
+
+	input:
+	output:
+	rules:
+	  explicit:
+	    -
+          implicit:
+	    -
+
+Examples and test cases:
+
+	merge([1, 3, 5], [3, 6, 9]) == [1, 3, 5, 6, 9]
+
+Data structure:
+
+	
+
+Algorithm:
+
+	
+
+=end
+
+
---
+++ RB101-RB119_Small_Problems/Easy_6/does_my_list_include_this.rb
@@ -0,0 +1,35 @@
+=begin 
+
+	Write a method named include? that takes an Array and a search value as arguments. This method should return true if the search value is in the array, false if it is not. You may not use the Array#include? method in your solution.
+
+PEDAC
+
+Problem:
+
+	input:
+	output:
+	rules:
+	  explicit:
+	    -
+          implicit:
+	    -
+
+Examples and test cases:
+
+	include?([1,2,3,4,5], 3) == true
+        include?([1,2,3,4,5], 6) == false
+        include?([], 3) == false
+        include?([nil], nil) == true
+        include?([], nil) == false
+
+Data structure:
+
+	
+
+Algorithm:
+
+	
+
+=end
+
+
