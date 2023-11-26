2395ffa493132c0c967190b0e8162d4eee25a37a
Loan Cardoso <jow13_122@yahoo.com>
2023-11-26 16:47:06 -0500

    rubocop compliance achieved

--- RB110/lesson_3/tictactoe.rb
+++ RB110/lesson_3/tictactoe.rb
@@ -1,31 +1,37 @@
 INITIAL_MARKER = ' '
 PLAYER_MARKER = 'X'
 COMPUTER_MARKER = 'O'
+WINNING_SEQUENCES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
+                    [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # columns
+                    [[1, 5, 9], [3, 5, 7]] # diagonals
 
 def prompt(msg)
   puts "=> #{msg}"
 end
 
+# rubocop:disable Metrics/AbcSize
 def display_board(brb)
+  puts "You're #{PLAYER_MARKER}. Computer is #{COMPUTER_MARKER}"
   system 'clear'
-  puts ' '*5 + '|' + ' '*5 + '|' + ' '*5
-  puts ' '*2 + brb[1] + ' '*2 + '|' + ' '*2 + brb[2] + ' '*2 + '|' + ' '*2 + brb[3] + ' '*2
-  puts ' '*5 + '|' + ' '*5 + '|' + ' '*5
-  puts '+'*5 + '+' + '+'*5 + '+' + '+'*5
-  puts ' '*5 + '|' + ' '*5 + '|' + ' '*5
-  puts ' '*2 + brb[4] + ' '*2 + '|' + ' '*2 + brb[5] + ' '*2 + '|' + ' '*2 + brb[6] + ' '*2
-  puts ' '*5 + '|' + ' '*5 + '|' + ' '*5
-  puts '+'*5 + '+' + '+'*5 + '+' + '+'*5
-  puts ' '*5 + '|' + ' '*5 + '|' + ' '*5
-  puts ' '*2 + brb[7] + ' '*2 + '|' + ' '*2 + brb[8] + ' '*2 + '|' + ' '*2 + brb[9] + ' '*2
-  puts ' '*5 + '|' + ' '*5 + '|' + ' '*5
+  puts "     |     |"
+  puts "  #{brb[1]}  |  #{brb[2]} |  #{brb[3]}"
+  puts "     |     |"
+  puts "-----+-----+-----"
+  puts "     |     |"
+  puts "  #{brb[4]}  |  #{brb[5]} |  #{brb[6]}"
+  puts "     |     |"
+  puts "-----+-----+-----"
+  puts "     |     |"
+  puts "  #{brb[7]}  |  #{brb[8]} |  #{brb[9]}"
+  puts "     |     |"
 end
+# rubocop:enable Metrics/AbcSize
 
 def initialize_board
   new_board = {}
-  (1..9).each { |n| new_board[n]= INITIAL_MARKER } 
+  (1..9).each { |n| new_board[n] = INITIAL_MARKER }
   new_board
-end 
+end
 
 def empty_squares(brb)
   brb.keys.reject { |num| brb[num] != INITIAL_MARKER }
@@ -33,32 +39,25 @@ end
 
 def player_places_piece(brb)
   square = 0
-  loop do 
+  loop do
     prompt "please choose a square (#{empty_squares(brb).join(',')}):"
     square = gets.chomp.to_i
     break if empty_squares(brb).include?(square)
     prompt "Invalid choice, try again."
   end
-  brb[square]= PLAYER_MARKER
+  brb[square] = PLAYER_MARKER
 end
 
 def computer_places_piece(brb)
   square = empty_squares(brb).sample
-  brb[square]= COMPUTER_MARKER
+  brb[square] = COMPUTER_MARKER
 end
 
 def detect_winner(brb)
-  winning_sequences = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
-                      [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # columns
-                      [[1, 5, 9], [3, 5, 7] ] # diagonals
-  winning_sequences.each do |seq|
-    if brb[seq[0]] == PLAYER_MARKER &&
-       brb[seq[1]] == PLAYER_MARKER &&
-       brb[seq[2]] == PLAYER_MARKER 
+  WINNING_SEQUENCES.each do |seq|
+    if brb.values_at(*seq).count(PLAYER_MARKER) == 3
       return 'Player'
-    elsif brb[seq[0]] == COMPUTER_MARKER &&
-	  brb[seq[1]] == COMPUTER_MARKER &&
-	  brb[seq[2]] == COMPUTER_MARKER 
+    elsif brb.values_at(*seq).count(COMPUTER_MARK) == 3
       return 'Computer'
     end
   end
@@ -73,7 +72,7 @@ def board_full?(brb)
   empty_squares(brb).empty?
 end
 
-loop do # main loop (multiple games) 
+loop do # main loop (multiple games)
   board = initialize_board
   display_board(board)
   loop do # single game loop
@@ -97,4 +96,3 @@ loop do # main loop (multiple games)
 end
 
 prompt "Thanks for playing Tic Tac Toe! Goodbye!"
-
