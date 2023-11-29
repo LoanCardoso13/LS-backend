e70ca41872cc4ba8bd0cbb900a74ee736bfaa7be
Loan Cardoso <jow13_122@yahoo.com>
2023-11-29 12:10:32 -0500

    refactored up until improving cards display interface

--- RB110/lesson_3/twentyone.rb
+++ RB110/lesson_3/twentyone.rb
@@ -1,23 +1,33 @@
-=begin
-
-1. Initialize deck
-2. Deal cards to player and dealer
-3. Player turn: hit or stay
-  - repeat until bust or "stay"
-4. If player bust, dealer wins.
-5. Dealer turn: hit or stay
-  - repeat until total >= 17
-6. If dealer bust, player wins.
-7. Compare cards and declare winner.
-
-=end
-
-ranks = [ 2, 3, 4, 5, 6, 7, 8, 9, 10, 'jack', 'queen', 'king', 'ace' ]
-deck = { heart: ranks.dup, diamond: ranks.dup, club: ranks.dup, spades: ranks.dup }
-score = [0, 0]
+RANKS = [ 2, 3, 4, 5, 6, 7, 8, 9, 10, 'jack', 'queen', 'king', 'ace' ]
+score = { Player: 0, Dealer: 0 }
 
 def prompt(msg)
   puts "=> #{msg}"
+end
+
+def hit_or_stay?
+  loop do
+    answer = gets.chomp
+    if answer.downcase.start_with?('h')
+      break false
+    elsif answer.downcase.start_with?('s')
+      break true
+    else
+      prompt "Not a valid answer..."
+      prompt "(H)it or (S)tay?"
+    end
+  end
+end
+
+def compare_cards(player, dealer)
+  if get_total(player) > get_total(dealer)
+    return 'player'
+  elsif get_total(dealer) > get_total(player)
+    return 'dealer'
+  else
+    prompt "It's a tie.."
+    return 'tie'
+  end
 end
 
 def get_total(hand)
@@ -45,6 +55,7 @@ def give_random_card(hand, deck)
 end
 
 loop do # Game loop (1 game consists of 2 turns; player's and dealer's, in this order)
+  deck = { hearts: RANKS.dup, diamonds: RANKS.dup, clubs: RANKS.dup, spades: RANKS.dup } # Reshuffling
   player = []
   dealer = []
   bust = false
@@ -56,19 +67,20 @@ loop do # Game loop (1 game consists of 2 turns; player's and dealer's, in this 
     2.times { |_| dealer << give_random_card(dealer, deck); player << give_random_card(player, deck) }
   end
   prompt "Welcome to the Twenty-One game!"
-  puts
   prompt "The goal of Twenty-One is to try to get as close to 21 as possible, without going over."
   prompt "If you go over 21, it's a 'bust' and you lose."
   puts
-  prompt "Dealer has: #{dealer.sample[:value]} and unknown card."
+  prompt "Current score: Player #{score[:Player]} x #{score[:Dealer]} Dealer."
+  puts
+  dealer_card = dealer.sample
+  prompt "Dealer has a #{dealer_card.values[0]} of #{dealer_card.keys[0]} (value: #{dealer_card[:value]}) and unknown card."
   loop do # Player turn
     prompt "You have: #{player.map { |hsh| hsh[:value] }.join(' and ')}"
     prompt "(H)it or (S)tay?"
-    answer = gets.chomp
-    break if answer.downcase.start_with?('s')
+    break if hit_or_stay?
     player << give_random_card(player, deck)
     prompt "You got a #{player.last[:value]}, your total is now #{get_total(player)}"
-    sleep(5)
+    sleep(1)
     if get_total(player) > 21
       prompt "You got 'bust'."
       winner = 'dealer'
@@ -77,31 +89,37 @@ loop do # Game loop (1 game consists of 2 turns; player's and dealer's, in this 
     end
   end
 
-  while get_total(dealer) < 17 do # Dealer turn
-    prompt "Dealer total is less than 17..."
-    sleep(1.3)
+  while get_total(dealer) < 17 && bust == false do # Dealer turn
+    prompt "Dealer total is less than 17 he decides to hit..."
+    sleep(1)
     dealer << give_random_card(dealer, deck)
     prompt "Dealer received #{dealer.last}"
     prompt "Dealer now has: #{dealer.map { |hsh| hsh[:value] }.join(' and ')}"
-    sleep(5)
-    break if get_total(dealer) > 17
+    sleep(1)
     if get_total(dealer) > 21
       prompt "Dealer got 'bust'."
       winner = 'player'
       bust = true
       break
     end
+    break if get_total(dealer) > 17
   end
 
   if bust == false
-    prompt "Both dealer and player have decided to stay, and no one got bust"
+    prompt "Dealer also decided to stay."
     prompt "It is now time to compare the cards..."
     prompt "Dealer total is: #{get_total(dealer)}"
     prompt "Player total is: #{get_total(player)}"
-    winner = get_total(player) > get_total(dealer) ? 'player' : 'dealer'
+    winner = compare_cards(player, dealer)
   end
-  sleep(5)
-  prompt "The winner is #{winner}."
+  sleep(1)
+  case winner
+  when 'player'
+    score[:Player] += 1
+  when 'dealer'
+    score[:Dealer] += 1
+  end
+  prompt "The winner is #{winner}." if winner != 'tie'
   prompt "Play again? (y/n)"
   answer = gets.chomp
   break unless answer.downcase.start_with?('y')
