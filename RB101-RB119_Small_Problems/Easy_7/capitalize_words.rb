0073ff5ba0d8c00d2630cb160dd3fab1a075cfcf
Loan Cardoso <jow13_122@yahoo.com>
2023-12-10 10:54:23 -0500

    refactoring selected exercises... (cont.)

--- RB101-RB119_Small_Problems/Easy_7/capitalize_words.rb
+++ RB101-RB119_Small_Problems/Easy_7/capitalize_words.rb
@@ -40,19 +40,11 @@ Algorithm
 
 =end
 
-def word_cap(str) # use a condition to give or take 32, or do nothing, to the ascii value of every character
-
-end
-
-def word_cap(str)
-  answer = str.split(' ').map do |word|
-    word.downcase!
-    word.capitalize!
-    word
-  end
-  answer.join(' ')
-end
+# def word_cap(str)
+#   str.split.map(&:capitalize).join(' ')
+# end
 
 puts word_cap('four score and seven') == 'Four Score And Seven'
 puts word_cap('the javaScript language') == 'The Javascript Language'
 puts word_cap('this is a "quoted" word') == 'This Is A "quoted" Word'
+# Refactored 1
