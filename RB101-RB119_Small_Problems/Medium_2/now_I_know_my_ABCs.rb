d77ae8c5acc9e342136b6f2653949b22e61aea51
Loan Cardoso <jow13_122@yahoo.com>
2023-12-16 12:53:45 -0500

    now_i_know_my_ABCs concluded

--- RB101-RB119_Small_Problems/Medium_2/now_I_know_my_ABCs.rb
+++ RB101-RB119_Small_Problems/Medium_2/now_I_know_my_ABCs.rb
@@ -15,11 +15,15 @@ PEDAC
 
 Problem:
 
-  input:
-  output:
+  input: a list of spelling blocks and a String word
+  output: Boolean true or false corresponding to the given word following rules defined by list of blocks or not
   rules:
     explicit:
+      - write a method
+      - words should not use use both letters of any given block
+      - words should not repeat a letter given in any block
     implicit:
+      - case-insensitive problem
 
 Examples and test cases:
 
@@ -29,6 +33,58 @@ Examples and test cases:
 
 Data structure:
 
+  Constant Array of Strings - 
+
 Algorithm:
 
+  Define constant BLOCKS to list of string blocks
+  Define method called block_word? with parameter: word
+  Reassign word to its uppercased version
+
+  Iterate through elements BLOCK
+    Define iterative variable ele
+    Initialize/reassign count1 and count2 to zero
+    Iterate through characters of word
+      Define iterative variable char
+      if first character of ele equals char, count1++
+      if last character of ele equals char, count2++
+      Return false if count1 or count2 equals 2
+
+  Iterate through elements of BLOCKS
+    Define iterative variable ele
+    Count how many characters of ele appear in word
+    Return false if that results to 2
+
+  true
+
 =end
+
+
+BLOCKS = [  'B:O',   'X:K',   'D:Q',   'C:P',   'N:A',
+  'G:T',   'R:E',   'F:S',   'J:W',   'H:U',
+  'V:I',   'L:Y',   'Z:M' ]
+
+def block_word?(word)
+  word = word.upcase
+  count = 0
+  
+  BLOCKS.each do |ele|
+    count1 = 0; count2 = 0
+    word.chars.each do |char|
+      count1 += 1 if ele[0] == char
+      count2 += 1 if ele[-1] == char
+      return false if count1 == 2 || count1 == 2
+    end
+  end
+
+  BLOCKS.each do |couple|
+    return false if word.count(couple) == 2
+  end
+
+  true
+end
+
+
+puts block_word?('BATCH') == true
+puts block_word?('BUTCH') == false
+puts block_word?('jest') == true
