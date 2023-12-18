50505be40ced895d349613568973195d3973d20a
Loan Cardoso <jow13_122@yahoo.com>
2023-12-18 10:54:24 -0500

    Advanced_1 list set, madlibs_revisited concluded

---
+++ RB101-RB119_Small_Problems/Advanced_1/merge_sorted_lists.rb
@@ -0,0 +1,31 @@
+# rubocop:disable all
+=begin
+
+  Write a method that takes two sorted Arrays as arguments, and returns a new Array that contains all elements from both arguments in sorted order.
+
+  You may not provide any solution that requires you to sort the result array. You must build the result array one element at a time in the proper order.
+
+  Your solution should not mutate the input arrays.
+
+PEDAC
+
+Problem:
+
+  input:
+  output:
+  rules:
+    explicit:
+    implicit:
+
+Examples and test cases:
+
+  merge([1, 5, 9], [2, 6, 8]) == [1, 2, 5, 6, 8, 9]
+  merge([1, 1, 3], [2, 2]) == [1, 1, 2, 2, 3]
+  merge([], [1, 4, 5]) == [1, 4, 5]
+  merge([1, 4, 5], []) == [1, 4, 5]
+
+Data structure:
+
+Algorithm:
+
+=end
