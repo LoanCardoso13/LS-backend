900e3e7ff54ca013bde7b1d27a748ca6af09cca9
Loan Cardoso <jow13_122@yahoo.com>
2024-01-10 18:53:15 -0500

    OO Easy_1 and Easy_2 lists set

---
+++ RB120_Object_Oriented_Programming/Easy_2/reverse_engineering.rb
@@ -0,0 +1,15 @@
+# rubocop:disable all
+=begin
+
+Write a class that will display:
+
+ABC
+xyz
+
+when the following code is run:
+
+my_data = Transform.new('abc')
+puts my_data.uppercase
+puts Transform.lowercase('XYZ')
+
+=end
