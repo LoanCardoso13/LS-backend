de8ffe02889d7fe2c0e14cff2922350625dfef27
Loan Cardoso <jow13_122@yahoo.com>
2024-02-03 13:54:53 -0500

    RB120 Easy_2 pet_shelter 2nd pass through

--- RB120_Object_Oriented_Programming/Easy_2/pet_shelter.rb
+++ RB120_Object_Oriented_Programming/Easy_2/pet_shelter.rb
@@ -1,76 +1,62 @@
-module Registrable
-  def number_of_pets
-    @pets.size
+# rubocop:disable all
+class Pet
+  attr_reader :name, :type
+
+  def initialize(type, name)
+    @name = name
+    @type = type
   end
+
 end
 
-class Shelter
-  attr_accessor :pets
-  attr_reader :name, :register
-
-  include Registrable
-
-  def initialize
-    @pets = []
-    @register = {}
-    @name = 'The Animal Shelter'
-  end
-
-  def adopt(owner = self, pet)
-    owner.pets << pet
-    if register.keys.include?(owner)
-      register[owner] << pet
-    else
-      register[owner] = [pet]
-    end
-  end
-
-  def print_adoptions
-    register.each do |owner, pets|
-      if owner.name == 'The Animal Shelter'
-        puts 'The Animal Shelter has the following unadopted pets:'
-        pets.each do |pet|
-          puts "a #{pet.type} named #{pet.name}"
-        end
-      else
-        puts "#{owner.name} has adopted the following pets:"
-        pets.each do |pet|
-          puts "a #{pet.type} named #{pet.name}"
-        end
-      end
-      puts
-    end
-  end
-end
 
 class Owner
-  attr_accessor :pets
   attr_reader :name
-
-  include Registrable
+  attr_accessor :pets
 
   def initialize(name)
     @name = name
     @pets = []
   end
-end
 
-class Pet
-  attr_reader :name, :type
-
-  def initialize(type, name)
-    @type = type
-    @name = name
+  def number_of_pets
+    @pets.length
   end
+
 end
 
-asta         = Pet.new('dog', 'Asta')
-laddie       = Pet.new('dog', 'Laddie')
-fluffy       = Pet.new('cat', 'Fluffy')
-kat          = Pet.new('cat', 'Kat')
-ben          = Pet.new('cat', 'Ben')
-chatterbox   = Pet.new('parakeet', 'Chatterbox')
-bluebell     = Pet.new('parakeet', 'Bluebell')
+class Shelter
+  attr_accessor :register
+
+  def initialize
+    @register = []
+  end
+
+  def adopt(owner, pet)
+    owner.pets << pet
+    register << owner if !register.include?(owner)
+  end
+
+  def print_adoptions
+    unadopted, adopted = register.partition { |owner| owner.name == 'The Animal Pet Shelter' }
+
+    puts "#{unadopted[0].name} has the following unadopted pets:"
+    unadopted[0].pets.each do |pet|
+      puts "A #{pet.type} named #{pet.name}"
+    end
+    puts 
+
+    adopted.each do |owner|
+      puts "#{owner.name} has adopted the following pets:"
+      owner.pets.each do |pet|
+        puts "A #{pet.type} named #{pet.name}"
+      end
+      puts
+    end
+  end
+
+end
+
 butterscotch = Pet.new('cat', 'Butterscotch')
 pudding      = Pet.new('cat', 'Pudding')
 darwin       = Pet.new('bearded dragon', 'Darwin')
@@ -78,18 +64,19 @@ kennedy      = Pet.new('dog', 'Kennedy')
 sweetie      = Pet.new('parakeet', 'Sweetie Pie')
 molly        = Pet.new('dog', 'Molly')
 chester      = Pet.new('fish', 'Chester')
+asta         = Pet.new('dog', 'Asta')
+laddie       = Pet.new('dog', 'Laddie')
+fluffy       = Pet.new('cat', 'Fluffy')
+kat          = Pet.new('cat', 'Kat')
+ben          = Pet.new('cat', 'Ben')
+chatterbox   = Pet.new('parakeet', 'Chatterbox')
+bluebell     = Pet.new('parakeet', 'Bluebell')
 
 phanson = Owner.new('P Hanson')
 bholmes = Owner.new('B Holmes')
+itself  = Owner.new('The Animal Pet Shelter')
 
 shelter = Shelter.new
-shelter.adopt(asta)
-shelter.adopt(laddie)
-shelter.adopt(fluffy)
-shelter.adopt(kat)
-shelter.adopt(ben)
-shelter.adopt(chatterbox)
-shelter.adopt(bluebell)
 shelter.adopt(phanson, butterscotch)
 shelter.adopt(phanson, pudding)
 shelter.adopt(phanson, darwin)
@@ -97,7 +84,14 @@ shelter.adopt(bholmes, kennedy)
 shelter.adopt(bholmes, sweetie)
 shelter.adopt(bholmes, molly)
 shelter.adopt(bholmes, chester)
+shelter.adopt(itself, asta)
+shelter.adopt(itself, laddie)
+shelter.adopt(itself, fluffy)
+shelter.adopt(itself, kat)
+shelter.adopt(itself, ben)
+shelter.adopt(itself, chatterbox)
+shelter.adopt(itself, bluebell)
 shelter.print_adoptions
 puts "#{phanson.name} has #{phanson.number_of_pets} adopted pets."
 puts "#{bholmes.name} has #{bholmes.number_of_pets} adopted pets."
-puts "#{shelter.name} has #{shelter.number_of_pets} unadopted pets."
+puts "#{itself.name} has #{itself.number_of_pets} unadopted pets."
