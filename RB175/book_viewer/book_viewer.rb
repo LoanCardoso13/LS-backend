b6c378d04f9bd6cabf7812fce40de7cc34503539
Loan Cardoso <jow13_122@yahoo.com>
2024-05-10 14:18:41 -0400

    enhanced book viewer sinatra app adding layout and search feature

--- RB175/book_viewer/book_viewer.rb
+++ RB175/book_viewer/book_viewer.rb
@@ -1,18 +1,69 @@
+# rubocop:disable all
 require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
 
+before do
+  @contents = File.readlines('data/toc.txt')
+end
+
+helpers do
+  def in_paragraphs(chp)
+    chp.split("\n\n").map.with_index { |par, idx| "<p id=#{idx}>#{par}</p>" }.join
+  end
+
+  def highlight_result(paragraph, searched)
+    paragraph.gsub(searched,"<strong>#{searched}</strong>")
+  end
+end
+
+not_found do
+  redirect "/"
+end
+
 get "/" do
   @title = 'The Adventures of Sherlock Holmes'
-  @contents = File.readlines('data/toc.txt')
 
   erb :home
 end
 
-get "/chapters/1" do
-  @title = 'Chapter 1'
-  @contents = File.readlines('data/toc.txt')
-  @chp1 = File.read('data/chp1.txt')
+get "/chapters/:number" do
+  number = params[:number].to_i
+  chp_name = @contents[number - 1]
+
+  redirect "/" unless (1..@contents.size).cover? number
+
+  @title = "Chapter #{number}: #{chp_name}"
+  @chapter = File.read("data/chp#{number}.txt")
 
   erb :chapter
+end
+
+def each_chapter
+  @contents.each_with_index do |name, idx|
+    number = idx + 1
+    paragraphs_arr = File.read("data/chp#{number}.txt").split("\n\n")
+    yield name, number, paragraphs_arr
+  end
+end
+
+def chapter_matching(query)
+  results = []
+
+  return results if !query || query.empty?
+
+  each_chapter do |name, number, paragraphs_arr|
+    matches = []
+    paragraphs_arr.each_with_index do |paragraph, idx|
+      matches << {content: paragraph, location: idx} if paragraph.include?(query)
+    end
+    results << {name:name, number:number, page:matches} unless matches.empty?
+  end
+
+  results
+end
+
+get "/search" do
+  @results = chapter_matching(params[:query])
+  erb :search
 end
