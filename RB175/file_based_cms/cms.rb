875d9a36459a1c5d2aade8bf5c0829e903d19d2e
Loan Cardoso <jow13_122@yahoo.com>
2024-06-01 15:33:17 -0400

    cms 2nd pass through until accessing session from test file

--- RB175/file_based_cms/cms.rb
+++ RB175/file_based_cms/cms.rb
@@ -8,21 +8,68 @@ def render_markdown(file_content)
   markdown.render(file_content)
 end
 
+def data_path
+  if ENV["RACK_ENV"] == "test"
+    File.expand_path("../test/data", __FILE__)
+  else
+    File.expand_path("../data", __FILE__)
+  end
+end
+
 configure do
   enable :sessions
   set :session_secret, SecureRandom.hex(32)
 end
 
-root = File.expand_path("..", __FILE__)
-
 get "/" do
-  @files_array = Dir.glob(root + '/data/*').map { |path| File.basename(path) }
+  if session[:user]
+    @files_array = Dir.glob(data_path + '/*').map { |path| File.basename(path) }
 
-  erb :index
+    erb :index
+  else
+    erb :signin
+  end
+end
+
+get "/users/signin" do
+  erb :signin
+end
+
+post "/users/signin" do
+  if params[:username] == "admin" && params[:password] == "secret"
+    session[:user] = "admin"
+    session[:message] = "Welcome!"
+    redirect "/"
+  else
+    session[:message] = "Invalid credentials"
+    erb :signin
+  end
+end
+
+post "/users/signout" do
+  session[:user] = nil
+  session[:message] = "You have been signed out."
+  redirect "/"
+end
+
+get "/new" do
+  erb :new_file
+end
+
+post "/new" do
+  file_name = params[:add]
+  unless file_name.strip.empty?
+    File.open(File.join(data_path, file_name), "w") 
+    session[:message] = "#{file_name} was created."
+    redirect "/"
+  else
+    session[:message] = "A name is required."
+    erb :new_file
+  end
 end
 
 get "/:file" do |file|
-  path = root + "/data/#{file}"
+  path = data_path + "/#{file}"
   if File.file? path
 
     case file[-3..]
@@ -46,7 +93,7 @@ get "/:file" do |file|
 end
 
 get "/:file/edit" do |file|
-  path = root + "/data/#{file}"
+  path = data_path + "/#{file}"
   @file = file
   @content = File.read(path)
 
@@ -55,8 +102,15 @@ end
 
 post "/:file/edit" do |file|
   session[:message] = "#{file} has been updated."
-  path = root + "/data/#{file}"
+  path = data_path + "/#{file}"
   File.write(path, params[:content])
   
+  redirect "/"
+end
+
+post "/:file/delete" do |file|
+  session[:message] = "#{file} has been deleted."
+  File.delete(File.join(data_path, file))
+
   redirect "/"
 end
--- RB175/file_based_cms/test/cms_test.rb
+++ RB175/file_based_cms/test/cms_test.rb
@@ -2,6 +2,7 @@ ENV["RACK_ENV"] = "test"
 
 require "minitest/autorun"
 require "rack/test"
+require "fileutils"
 
 require_relative "../cms"
 
@@ -10,6 +11,27 @@ class AppTest < Minitest::Test
 
   def app
     Sinatra::Application
+  end
+
+  def create_document(name, content = "")
+    File.open(File.join(data_path, name), "w") do |file|
+      file.write(content)
+    end
+  end
+
+  def setup
+    root = File.expand_path("../../data", __FILE__)
+    content1 = File.read(root + '/about.md')
+    content2 = File.read(root + '/changes.txt')
+    content3 = File.read(root + '/history.txt')
+    FileUtils.mkdir_p(data_path)
+    create_document "about.md", content1
+    create_document "changes.txt", content2
+    create_document "history.txt", content3
+  end
+
+  def teardown
+    FileUtils.rm_rf(data_path)
   end
 
   def test_index
@@ -50,7 +72,99 @@ class AppTest < Minitest::Test
   end
 
   def test_edit
-    
+    get "/history.txt/edit"    
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, "<textarea"
+
+    post "/history.txt/edit", content: "Added content for testing."
+    assert_equal 302, last_response.status
+
+    redirection = last_response["location"]
+    get redirection
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, "history.txt has been updated."
+
+    get redirection
+    assert_equal 200, last_response.status
+    refute_includes last_response.body, "history.txt has been updated."
+
+    get "/history.txt"
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, "Added content for testing." 
+  end
+
+  def test_create
+    get "/new"
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, "<button" 
+
+    post "/new", add: 'dummy.txt'
+    assert_equal 302, last_response.status
+
+    get last_response["location"]
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, "dummy.txt" 
+    assert_includes last_response.body, "dummy.txt was created" 
+
+    post "/new", add: '   '
+    assert_includes last_response.body, "A name is required." 
+  end
+
+  def test_delete
+    get "/"
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, ">Delete<"
+    assert_includes last_response.body, "history.txt"
+    assert_includes last_response.body, "about.md"
+
+    post "/history.txt/delete"
+    assert_equal 302, last_response.status
+
+    redirection = last_response["location"]
+    get redirection
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, "history.txt has been deleted."
+    assert_includes last_response.body, "about.md"
+
+    get redirection
+    assert_equal 200, last_response.status
+    refute_includes last_response.body, "history.txt has been deleted."
+    assert_includes last_response.body, "about.md"
+  end
+
+  def test_signin_form
+    get "/users/signin"
+
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, "<input"
+    assert_includes last_response.body, %q(<button type="submit")
+  end
+
+  def test_signin
+    post "/users/signin", username: "admin", password: "secret"
+    assert_equal 302, last_response.status
+
+    get last_response["Location"]
+    assert_includes last_response.body, "Welcome"
+    assert_includes last_response.body, "Signed in as admin"
+  end
+
+  def test_signin_with_bad_credentials
+    post "/users/signin", username: "guest", password: "shhhh"
+    assert_equal 422, last_response.status
+    assert_includes last_response.body, "Invalid credentials"
+  end
+
+  def test_signout
+    post "/users/signin", username: "admin", password: "secret"
+    get last_response["Location"]
+    assert_includes last_response.body, "Welcome"
+
+    post "/users/signout"
+    get last_response["Location"]
+
+    assert_includes last_response.body, "You have been signed out"
+    assert_includes last_response.body, "Sign In"
   end
 
 end
